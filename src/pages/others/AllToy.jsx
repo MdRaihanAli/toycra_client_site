f181e81d2d655f0960f213527395705ba1430634
MdRaihanAli <raihanraju43@gmail.com>
2023-05-20 09:38:18 +0400

    all toys pase fines

--- src/pages/others/AllToy.jsx
+++ src/pages/others/AllToy.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function AllToy() {
     const [toy, setToy] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/galery')
+        fetch('http://localhost:5000/alltoy')
             .then(res => res.json())
             .then(data => {
                 setToy(data)
@@ -13,36 +13,39 @@ function AllToy() {
             })
     }, [])
     return (
-        <div className='container'>
+        <div className="bg_color">
+            <h3 className="text-bold text-center py-4"> All <span className="tomato_color">Toys</span></h3>
+            <div className='container'>
 
-            <Table responsive="sm">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Imaige</th>
-                        <th>Toy Name</th>
-                        <th>Sheler Name</th>
-                        <th>Sub-category</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Details </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        toy.map((x, i) => <tr key={i}>
-                            <td>{i}</td>
-                            <td><img style={{widows:'80px', height:'80px'}} src={x.photo} alt="" /></td>
-                            <td>{x.title}</td>
-                            <td>{x.seler}</td>
-                            <td>{x.category}</td>
-                            <td>{x.price}</td>
-                            <td>{x.quantity}</td>
-                            <td> <Link to={`/viewdetails/${x._id}`}className='btn btn-secondary'>Details</Link></td>
-                        </tr>)
-                    }
-                </tbody>
-            </Table>
+                <Table striped bordered hover responsive="sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Imaige</th>
+                            <th>Toy Name</th>
+                            <th>Sheler Name</th>
+                            <th>Sub-category</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Details </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            toy.map((x, i) => <tr key={i}>
+                                <td>{i + 1}</td>
+                                <td><img style={{ widows: '80px', height: '80px' }} src={x.photo} alt="" /></td>
+                                <td>{x.title}</td>
+                                <td>{x.seler}</td>
+                                <td>{x.category}</td>
+                                <td>{x.price}</td>
+                                <td>{x.quantity}</td>
+                                <td> <Link to={`/viewdetails/${x._id}`} className='btn btn-secondary'>Details</Link></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </Table>
+            </div>
         </div>
     )
 }
